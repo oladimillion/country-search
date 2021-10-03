50bfa33f2b3327f8756b0d0023469d4f74af8469
oladimillion <akandeoladimeji9@hotmail.com>
2021-10-03 22:36:48 +0100

    add pre-stage linting

--- server/index.ts
+++ server/index.ts
@@ -1,38 +1,37 @@
-import express from "express"
-import log from "./helper/logger"
-import db from "./db"
-import router from "./router"
-import headerToken from "./middleware/headerToken"
-import requestLogger from "./middleware/requestLogger"
-import cors from "./middleware/cors"
-import rateLimiter from "./middleware/rateLimiter"
+import express from "express";
+import log from "./helper/logger";
+import db from "./db";
+import router from "./router";
+import headerToken from "./middleware/headerToken";
+import requestLogger from "./middleware/requestLogger";
+import cors from "./middleware/cors";
+import rateLimiter from "./middleware/rateLimiter";
 
 // loading .env variables
-require("dotenv").config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT as string
+const PORT = process.env.PORT as string;
 
 app.use(cors);
 
-app.disable('x-powered-by');
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.disable("x-powered-by");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // limit max request per minute to 30
-app.use(rateLimiter)
+app.use(rateLimiter);
 
 // deserialize header token
-app.use(headerToken)
+app.use(headerToken);
 
 // log every request
-app.use(requestLogger)
+app.use(requestLogger);
 
-app.use(router)
+app.use(router);
 
 app.listen(PORT, () => {
-  log.info(`Server listening on http://localhost:${PORT}`)
-  db.connect()
-})
-
+  log.info(`Server listening on http://localhost:${PORT}`);
+  db.connect();
+});
